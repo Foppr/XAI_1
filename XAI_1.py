e5d84fa90dc3bf0c90ed64c44c4c1de12e0a2006
Foppe <foppe02@gmail.com>
2026-03-01 23:52:34 +0100

    sleep

--- XAI_1.py
+++ XAI_1.py
@@ -7,7 +7,7 @@ from bokeh.models import CustomJS, Dropdown, ColumnDataSource, FactorRange, GeoJ
 # from bokeh.sampledata.sample_geojson import geojson
 import json
 import matplotlib
-import pgeocode
+# import pgeocode
 
 from shapely.geometry import Polygon
 
@@ -18,6 +18,8 @@ from datetime import datetime
 c = CurrencyConverter(fallback_on_missing_rate=True)
 io.output_file('Dashboard.html')
 
+# region pre-processing
+
 # --------------- SALES --------------------------------------------------
 
 path_name = Path('assignment1_data').glob(r'sales*10*')
@@ -26,19 +28,16 @@ for csv in path_name:
     df = pd.read_csv(csv, encoding='utf-8', sep=',')
     sales.append(df)
 
-# for csv in lst:
-#     print(csv.to_string())
-
 # The formats of sales files 6 and 7 are different than those in 1-5
 # CHANGE 67 NAMES TO 1-5 NAMES
 for csv in ['assignment1_data/sales_202111.csv', 'assignment1_data/sales_202112.csv']:
     df = pd.read_csv(csv, encoding='utf-8', sep=',')
     df = df.rename(columns={
-            'Order Charged Date': 'Transaction Date',
-            'Product ID': 'Product id',
-            'SKU ID': 'Sku Id',
-            'Country of Buyer': 'Buyer Country',
-            'Postal Code of Buyer': 'Buyer Postal Code',
+        'Order Charged Date': 'Transaction Date',
+        'Product ID': 'Product id',
+        'SKU ID': 'Sku Id',
+        'Country of Buyer': 'Buyer Country',
+        'Postal Code of Buyer': 'Buyer Postal Code',
     })
 
     # Note: November and December only have Charged Amount, which is in the original currency and not in EUR,
@@ -52,7 +51,8 @@ for csv in ['assignment1_data/sales_202111.csv', 'assignment1_data/sales_202112.
     df['Transaction Date'] = df['Transaction Date'].apply(lambda x: datetime.strptime(f"{x}", "%Y-%m-%d"))
     df['Transaction Date'] = df['Transaction Date'].apply(lambda x: x.strftime("%b %d, %Y"))
 
-    df['Charged Amount'] = df['Charged Amount'].apply(lambda x: float(x.replace(',', '')) if isinstance(x, str) else float(x))
+    df['Charged Amount'] = df['Charged Amount'].apply(
+        lambda x: float(x.replace(',', '')) if isinstance(x, str) else float(x))
 
     # Change currency to EUR
     # df['Amount (Merchant Currency)'] = c.convert(df['Amount (Merchant Currency)'], 'EUR', df['Currency of Sale'], date=df['Transaction Date'])
@@ -108,7 +108,6 @@ for csv in path_name:
 
 crashes_db = pd.concat(stats_crashes, ignore_index=True)
 
-
 # --------------- RATINGS COUNTRY --------------------------------------------------
 path_name = Path('assignment1_data').glob(r'stats_ratings*country*')
 ratings_countries = []
@@ -119,7 +118,7 @@ for csv in path_name:
 ratings_countries_db = pd.concat(ratings_countries, ignore_index=True)
 # print(ratings_countries_db[:20].to_string())
 
-#--------------- RATINGS OVERVIEW --------------------------------------------------
+# --------------- RATINGS OVERVIEW --------------------------------------------------
 path_name = Path('assignment1_data').glob(r'stats_ratings*overview*')
 ratings_overview = []
 for csv in path_name:
@@ -128,7 +127,11 @@ for csv in path_name:
 
 ratings_overview_db = pd.concat(ratings_overview, ignore_index=True)
 
-# --------------- DASHBOARD --------------------------------------------------
+# endregion pre-processing
+
+# region data collecting
+
+# --------------- Collect data for plots --------------------------------------------------
 # Sales
 days = []
 months = []
@@ -137,10 +140,14 @@ daily_merchant_amount = {}
 daily_transaction_count = {}
 monthly_merchant_amount = {}
 monthly_transaction_count = {}
+total_monthly_sales = {}
 sku_id_per_month_amount = {}
 sku_id_per_month_count = {}
+
 for i, row in sales_db.iterrows():
+    country = row['Buyer Country']
     date = row['Transaction Date']
+
     if date not in days:
         days.append(date)
         daily_merchant_amount[date] = float(row['Amount (Merchant Currency)'])
@@ -151,16 +158,30 @@ for i, row in sales_db.iterrows():
 
     if date[:3] not in months:
         months.append(date[:3])
+        month_no = months.index(date[:3])  # 0 for jun, 1 for jul, etc.
+
         monthly_merchant_amount[date[:3]] = float(row['Amount (Merchant Currency)'])
         monthly_transaction_count[date[:3]] = 1
 
-        sku_id = row['Sku Id']
+        # Add to total_monthly_sales
+        if country not in total_monthly_sales:
+            total_monthly_sales[country] = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+            # [total_sales, total_transaction_count, total_sale per transaction count]
+            total_monthly_sales[country][month_no][0] = float(row['Amount (Merchant Currency)'])
+            total_monthly_sales[country][month_no][1] = 1
+        else:
+            total_monthly_sales[country][month_no][0] += float(row['Amount (Merchant Currency)'])
+            total_monthly_sales[country][month_no][1] += 1
 
+        total_monthly_sales[country][month_no][2] = total_monthly_sales[country][month_no][0] / \
+                                                    total_monthly_sales[country][month_no][1]
+
+        # Sku IDs
+        sku_id = row['Sku Id']
         for sku_dict in [sku_id_per_month_amount, sku_id_per_month_count]:
             for sku_id_list in sku_dict.values():
                 while len(sku_id_list) != len(months):
                     sku_id_list.append(0)
-
         if sku_id not in sku_id_per_month_amount:
             sku_id_per_month_amount[sku_id] = [float(row['Amount (Merchant Currency)'])]
             sku_id_per_month_count[sku_id] = [1]
@@ -168,12 +189,26 @@ for i, row in sales_db.iterrows():
         else:
             sku_id_per_month_amount[sku_id][-1] += float(row['Amount (Merchant Currency)'])
             sku_id_per_month_count[sku_id][-1] += 1
+
     else:
         monthly_merchant_amount[date[:3]] += float(row['Amount (Merchant Currency)'])
         monthly_transaction_count[date[:3]] += 1
 
-        sku_id = row['Sku Id']
+        # Add to total_monthly_sales
+        if country not in total_monthly_sales:
+            total_monthly_sales[country] = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+            # [total_sales, total_transaction_count, total_sale per transaction count]
+            total_monthly_sales[country][month_no][0] = float(row['Amount (Merchant Currency)'])
+            total_monthly_sales[country][month_no][1] = 1
+        else:
+            total_monthly_sales[country][month_no][0] += float(row['Amount (Merchant Currency)'])
+            total_monthly_sales[country][month_no][1] += 1
 
+        total_monthly_sales[country][month_no][2] = total_monthly_sales[country][month_no][0] / \
+                                                    total_monthly_sales[country][month_no][1]
+
+        # Sku IDs
+        sku_id = row['Sku Id']
         if sku_id not in sku_id_per_month_amount:
             sku_id_per_month_amount[sku_id] = [float(row['Amount (Merchant Currency)'])]
             sku_id_per_month_count[sku_id] = [1]
@@ -181,13 +216,20 @@ for i, row in sales_db.iterrows():
         else:
             sku_id_per_month_amount[sku_id][-1] += float(row['Amount (Merchant Currency)'])
             sku_id_per_month_count[sku_id][-1] += 1
+
 # print months
 # for date, merchant_amount in monthly_merchant_amount.items():
 #     print(date, merchant_amount)
 #
 # for date, transaction_count in monthly_transaction_count.items():
 #     print(date, transaction_count)
+#
+# for e in total_monthly_sales.items():
+#     print(e)
 
+# endregion data collecting
+
+# region dashboard
 sales_source = ColumnDataSource(data=sales_db)
 x = months
 
@@ -205,14 +247,11 @@ p3 = plotting.figure(x_range=x, title='Daily Sales')
 y = list(daily_merchant_amount.values())
 p3.line(x, y)
 
-
 p4 = plotting.figure(x_range=x, title='Daily Transaction Counts')
 y = list(daily_transaction_count.values())
 p4.line(x, y)
 
-
-# For sku_id seperation:
-
+# Sales volume by Sku ID
 data_amount = {"months": months}
 for key, value in sku_id_per_month_amount.items():
     data_amount[key] = value
@@ -224,13 +263,13 @@ for key, value in sku_id_per_month_count.items():
 source = ColumnDataSource(data=data_amount)
 
 p1sku = plotting.figure(x_range=months, title="Monthly Sales by Sku Id",
-                    height=350, toolbar_location=None, tools="")
+                        height=350, toolbar_location=None, tools="")
 
 p1sku.vbar(x=transform.dodge('months', -0.25, range=p1sku.x_range), top='unlockcharactermanager', source=source,
-       width=0.2, color="#c9d9d3", legend_label="unlockcharactermanager")
+           width=0.2, color="#c9d9d3", legend_label="unlockcharactermanager")
 
 p1sku.vbar(x=transform.dodge('months', 0.0, range=p1sku.x_range), top='premium', source=source,
-       width=0.2, color="#718dbf", legend_label="premium")
+           width=0.2, color="#718dbf", legend_label="premium")
 
 p1sku.x_range.range_padding = 0.1
 p1sku.xgrid.grid_line_color = None
@@ -240,29 +279,66 @@ p1sku.legend.orientation = "horizontal"
 source = ColumnDataSource(data=data_count)
 
 p2sku = plotting.figure(x_range=months, title="Monthly Transaction Counts by Sku Id",
-                     height=350, toolbar_location=None, tools="")
+                        height=350, toolbar_location=None, tools="")
 
 p2sku.vbar(x=transform.dodge('months', -0.25, range=p2sku.x_range), top='unlockcharactermanager', source=source,
-        width=0.2, color="#c9d9d3", legend_label="unlockcharactermanager")
+           width=0.2, color="#c9d9d3", legend_label="unlockcharactermanager")
 
 p2sku.vbar(x=transform.dodge('months', 0.0, range=p2sku.x_range), top='premium', source=source,
-        width=0.2, color="#718dbf", legend_label="premium")
+           width=0.2, color="#718dbf", legend_label="premium")
 
 p2sku.x_range.range_padding = 0.1
 p2sku.xgrid.grid_line_color = None
 p2sku.legend.location = "top_left"
 p2sku.legend.orientation = "horizontal"
 
-graphs = [p1, p2, p3, p4, p1sku, p2sku]
-cols = []
-row_num = 2
-for i in range(0, len(graphs), row_num):
-    r = layouts.row(graphs[i : i + row_num])
-    cols.append(r)
+# Sales volume by country
+relevant_countries = ['GB', 'NL', 'AU', 'CA', 'DE']
+
+transaction_counts = {'months': months}
+sales_by_transaction_count = {'months': months}
+for country in relevant_countries:
+    transaction_counts[country] = [sales[1] for sales in total_monthly_sales[country]]
+    sales_by_transaction_count[country] = [sales[2] for sales in total_monthly_sales[country]]
+
+x = [(country, month) for country in relevant_countries for month in months]
+counts = ((transaction_counts['GB']) +
+          (transaction_counts['NL']) +
+          (transaction_counts['AU']) +
+          (transaction_counts['CA']) +
+          (transaction_counts['DE']))
+
+sales_by_count = ((sales_by_transaction_count['GB']) +
+                  (sales_by_transaction_count['NL']) +
+                  (sales_by_transaction_count['AU']) +
+                  (sales_by_transaction_count['CA']) +
+                  (sales_by_transaction_count['DE']))
+
+source_counts = ColumnDataSource(data=dict(x=x, counts=counts))
+source_sales_by_count = ColumnDataSource(data=dict(x=x, counts=sales_by_count))
+
+pcountries = plotting.figure(x_range=FactorRange(*x), height=350, title="Monthly transaction counts by country",
+                             toolbar_location=None, tools="")
+pcountries_salesbycount = plotting.figure(x_range=FactorRange(*x), height=350, title="Monthly average revenue per transaction",
+                             toolbar_location=None, tools="")
+
+pcountries.vbar(x='x', top='counts', width=0.9, source=source_counts)
+pcountries_salesbycount.vbar(x='x', top='counts', width=0.9, source=source_sales_by_count)
+
+pcountries.y_range.start = 0
+pcountries.x_range.range_padding = 0.1
+pcountries.xaxis.major_label_orientation = 1
+pcountries.xgrid.grid_line_color = None
+
+pcountries_salesbycount.y_range.start = 0
+pcountries_salesbycount.x_range.range_padding = 0.1
+pcountries_salesbycount.xaxis.major_label_orientation = 1
+pcountries_salesbycount.xgrid.grid_line_color = None
+
+# plotting.show(pcountries)
 
 # crashes_source = ColumnDataSource(data=stats_db)
 # ratings_countries_source = ColumnDataSource(data=ratings_countries_db)
-
 
 # crashes:
 
@@ -276,39 +352,34 @@ for i, row in ratings_overview_db.iterrows():
     ratings_list.append(row["Daily Average Rating"])
     crashes_list.append(crashes_db.iloc[i]["Daily Crashes"])
 
-rxc = plotting.figure(title = "Daily Average Rating by Daily Crashes")
+rxc = plotting.figure(title="Daily Average Rating by Daily Crashes")
 
 # points to be plotted
 par = np.polyfit(ratings_list, crashes_list, 1, full=True)
-slope=par[0][0]
-intercept=par[0][1]
-y_predicted = [slope*i + intercept for i in ratings_list]
+slope = par[0][0]
+intercept = par[0][1]
+y_predicted = [slope * i + intercept for i in ratings_list]
 
 # plotting the graph
 rxc.scatter(ratings_list, crashes_list)
 # plot regression line
-rxc.line(ratings_list,y_predicted, color='red')
+rxc.line(ratings_list, y_predicted, color='red')
 
 # displaying the model
-graphs = [p1, p2, p3, p4, p1sku, p2sku, rxc]
+graphs = [p1, p2, p3, p4, p1sku, p2sku, rxc, pcountries, pcountries_salesbycount]
 cols = []
 row_num = 2
 for i in range(0, len(graphs), row_num):
-    r = layouts.row(graphs[i : i + row_num])
+    r = layouts.row(graphs[i: i + row_num])
     cols.append(r)
 plotting.show(layouts.column(cols))
 
+# endregion dashboard
 
-# path_to_data = get_path("country_data/ne_110m_admin_0_countries.shp")
-# print(list(world.keys()))
-# Source - https://stackoverflow.com/a/56110989
-# Posted by Tony
-# Retrieved 2026-03-01, License - CC BY-SA 4.0
-
-
+# region countries
 # ------------------------------------- COUNTRIES -------------------------------------
 
-#---------------------------------------------
+# ---------------------------------------------
 # world = geopandas.read_file("country_data/ne_110m_admin_0_countries.shp")
 
 # cities = geopandas.read_file(geodatasets.get_path("naturalearth.land"))
@@ -357,33 +428,33 @@ plotting.show(layouts.column(cols))
 # print(x_axis, y_axis)
 
 
-#-------- find emerging countries
+# -------- find emerging countries
 # find countries with highest sales growth:
 # total sales last 2 months divided by total sales first 2 months -1
 
-growth = {}
-for i, row in sales_db.iterrows():
-    country = row["Buyer Country"]
-    date = row['Transaction Date']
-    if country not in growth:
-        growth[country] = [0, 0, 0] # sales first month, sales second month, growth
-    if months.index(date[:3]) <= 1:
-        growth[country][0] += float(row['Amount (Merchant Currency)'])
-    elif months.index(date[:3]) >= len(months)-2:
-        growth[country][1] += float(row['Amount (Merchant Currency)'])
-
-print(growth)
-for country in growth.keys():
-    if growth[country][0] == 0:
-        growth[country][0] = 1
-    growth[country][2] = (growth[country][1] / growth[country][0]) - 1
-
-ranking = sorted(growth.items(), key=lambda x: x[1][2], reverse=True)
-
-for rank, (country, values) in enumerate(ranking, start=1):
-    g = values[2]
-    print(f"{rank}. {country} - Growth: {g:.2f} (Months 1-2 vs 6-7: {values[0]}, {values[1]})")
+# endregion countries
 
 
-
-
+# growth = {}
+# for i, row in sales_db.iterrows():
+#     country = row["Buyer Country"]
+#     date = row['Transaction Date']
+#     if country not in growth:
+#         growth[country] = [0, 0, 0]  # sales first month, sales second month, growth
+#
+#     if months.index(date[:3]) <= 1:
+#         growth[country][0] += float(row['Amount (Merchant Currency)'])
+#     elif months.index(date[:3]) >= len(months) - 2:
+#         growth[country][1] += float(row['Amount (Merchant Currency)'])
+#
+# print(growth)
+# for country in growth.keys():
+#     if growth[country][0] == 0:
+#         growth[country][0] = 1
+#     growth[country][2] = (growth[country][1] / growth[country][0]) - 1
+#
+# ranking = sorted(growth.items(), key=lambda x: x[1][2], reverse=True)
+#
+# for rank, (country, values) in enumerate(ranking, start=1):
+#     g = values[2]
+#     print(f"{rank}. {country} - Growth: {g:.2f} (Months 1-2 vs 6-7: {values[0]}, {values[1]})")
